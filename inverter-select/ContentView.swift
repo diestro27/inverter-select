e7e6df81fda7f5117148e6d51d076c3449b044ff
Dexter Berend <dexter.berend@sol-ark.com>
2024-05-16 17:53:04 -0500

    basic layout of the first screen

--- inverter-select/ContentView.swift
+++ inverter-select/ContentView.swift
@@ -8,12 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = "";
+  @State private var inverter = "15K-2P-N";
+  
+  let inverters = ["30K-3P-208V-N", "15K-2P-N", "12K-2P-N", "5K-1P-N"];
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("logo-official")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 250, height: 100.0)
+                .padding(.all, 16)
+            Section {
+                TextField("Name", text: $name)
+                  .border(Color.orange)
+                  .frame(maxWidth: .infinity)
+                  .multilineTextAlignment(.center)
+                  .padding(.horizontal, UIScreen.main.bounds.width * 0.1)
+              Picker("Inverter", selection: $inverter) {
+                ForEach(inverters, id: \.self) {
+                  Text($0)
+                }
+              }
+            }
+          
+          Button(action: {
+            print("Submit button tapped with name: \(name) and inverter: \(inverter).")
+          }) {
+            Text("Submit")
+              .padding()
+              .frame(maxWidth: .infinity)
+              .foregroundColor(.white)
+              .background(Color.blue)
+              .cornerRadius(10)
+          }
+          .frame(width: 140)
+          .padding(.top, 16)
         }
         .padding()
     }
