2a17c71cfae3c331a3ef0009f32cf94323452a86
Dexter Berend <dexter.berend@sol-ark.com>
2024-05-16 16:37:03 -0500

    Initial Commit

---
+++ inverter-select/inverter_selectApp.swift
@@ -0,0 +1,17 @@
+//
+//  inverter_selectApp.swift
+//  inverter-select
+//
+//  Created by Dexter Berend on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct inverter_selectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
